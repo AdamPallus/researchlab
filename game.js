acd75320d012695f9fc01c0e29ebc90dd8a8d2b7
Adam Pallus <pallusa@gmail.com>
2023-11-04 14:52:00 -0700

    Adding grant functionality

--- game.js
+++ game.js
@@ -2,21 +2,38 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const researchButton = document.getElementById('research-button');
-    const availablePapersElement = document.getElementById('available-papers');
-    const lifetimePapersElement = document.getElementById('lifetime-papers');
-    const labFundingElement = document.getElementById('lab-funding');
     const grantsListElement = document.getElementById('grants-list');
 
+    // Event listener for the "Perform Research" button
     researchButton.addEventListener('click', () => {
         performResearch();
     });
 
-    /**
-     * Updates the game display with the current state
-     */
+    // Function to update the game display with the current state
     window.updateDisplay = function() {
+        const availablePapersElement = document.getElementById('available-papers');
+        const lifetimePapersElement = document.getElementById('lifetime-papers');
+        const labFundingElement = document.getElementById('lab-funding');
+
         availablePapersElement.textContent = state.availablePapers;
         lifetimePapersElement.textContent = state.lifetimePapers;
-        labFundingElement.textContent = state.labFunding;
-    }
+        labFundingElement.textContent = state.labFunding.toFixed(2);
+    };
+
+    // Function to update the grants display
+    window.updateGrantDisplay = function() {
+        grantsListElement.innerHTML = ''; // Clear the current list
+        state.grantOpportunities.forEach((grant, index) => {
+            const grantElement = document.createElement('li');
+            grantElement.textContent = `Grant ${index + 1}: $${grant.fundingAmount} (Cost: ${grant.cost} papers, Chance: ${grant.chance}%)`;
+            grantElement.onclick = () => applyForGrant(index);
+            grantsListElement.appendChild(grantElement);
+        });
+    };
+
+    // Periodically check for new grant opportunities
+    setInterval(() => {
+        generateGrants();
+        updateDisplay();
+    }, 5000); // Check every 5 seconds
 });
--- state.js
+++ state.js
@@ -1,10 +1,14 @@
-// Game state management
+// Game state management with grant generation and application functionality
 
 let state = {
     availablePapers: 0,
     lifetimePapers: 0,
     labFunding: 0,
     grantOpportunities: [],
+    grantBaseAmount: 1000, // Starting funding amount for grants
+    grantPaperCost: 10, // Default cost in papers for a grant
+    grantSuccessChance: 50, // Starting success chance for grants
+    grantDecisionTime: 10000 // Decision time in milliseconds for grants
 };
 
 /**
@@ -23,11 +27,14 @@ function applyForGrant(grantIndex) {
     let grant = state.grantOpportunities[grantIndex];
     if (state.availablePapers >= grant.cost) {
         state.availablePapers -= grant.cost;
+        // Remove the grant from the opportunities list
+        state.grantOpportunities.splice(grantIndex, 1);
         setTimeout(() => {
             if (isGrantSuccessful(grant.chance)) {
                 state.labFunding += grant.fundingAmount;
             }
             updateDisplay();
+            updateGrantDisplay();
         }, grant.decisionTimeline);
     } else {
         alert('Not enough research papers!');
@@ -36,8 +43,51 @@ function applyForGrant(grantIndex) {
 }
 
 /**
+ * Creates a new grant opportunity
+ */
+function createGrantOpportunity() {
+    const grant = {
+        fundingAmount: state.grantBaseAmount,
+        cost: state.grantPaperCost,
+        chance: state.grantSuccessChance,
+        decisionTimeline: state.grantDecisionTime
+    };
+    state.grantOpportunities.push(grant);
+    updateGrantDisplay();
+}
+
+/**
+ * Generates grant opportunities if conditions are met
+ */
+function generateGrants() {
+    if (state.availablePapers >= 10 && state.grantOpportunities.length < 5) {
+        createGrantOpportunity();
+    }
+}
+
+/**
  * Updates the game display with the current state
  */
 function updateDisplay() {
-    // This function will interact with the DOM, to be implemented in game.js
+    const availablePapersElement = document.getElementById('available-papers');
+    const lifetimePapersElement = document.getElementById('lifetime-papers');
+    const labFundingElement = document.getElementById('lab-funding');
+
+    availablePapersElement.textContent = state.availablePapers;
+    lifetimePapersElement.textContent = state.lifetimePapers;
+    labFundingElement.textContent = state.labFunding.toFixed(2);
+}
+
+/**
+ * Updates the grants display with the current state
+ */
+function updateGrantDisplay() {
+    const grantsListElement = document.getElementById('grants-list');
+    grantsListElement.innerHTML = ''; // Clear the current list
+    state.grantOpportunities.forEach((grant, index) => {
+        const grantElement = document.createElement('li');
+        grantElement.textContent = `Grant ${index + 1}: $${grant.fundingAmount} (Cost: ${grant.cost} papers, Chance: ${grant.chance}%)`;
+        grantElement.onclick = () => applyForGrant(index);
+        grantsListElement.appendChild(grantElement);
+    });
 }
